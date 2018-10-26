b63439e8cc687bb95bb37613daa4da3e655de50f
YujiangZhou <43716881+YujiangZhou@users.noreply.github.com>
2018-10-26 14:49:55 +0800

    Update VarietyStudy/zhouyujiang .cpp

---
+++ VarietyStudy/zhouyujiang .cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=1;
+	float y = 2;
+	char sb = 'A';
+	unsigned int a = 464566;
+	short int b = 456;
+	long int c = 45452;
+
+	cout <<"x="<< x<<endl<<"y="<<y<<endl<<"sb="<<sb<<endl;
+	cout << "a=" << a << endl << "b=" << b <<endl<< "c=" << c << endl;
+	system("pause");
+	return 0;
+}
